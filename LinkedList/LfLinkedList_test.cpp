685313163a4bcbbf786a3fdb258d75a1601ba570
luohaha <18810541851@163.com>
2021-01-17 13:42:59 +0800

    add LinkedList

---
+++ LinkedList/LfLinkedList_test.cpp
@@ -0,0 +1,39 @@
+#include "LfLinkedList.h"
+#include<vector>
+#include<thread>
+
+using namespace Lf;
+
+LinkedList list;
+
+void RandomSearchTest(int index) {
+  for (int i = 0; i < 10000; i++) {
+    Node node;
+    Node ret;
+    uint32_t r = std::rand();
+    node.key = std::to_string(index) + "_" + std::to_string(r);
+    node.value = std::to_string(r);
+    list.Insert(node);
+    assert(list.Search(node, ret) == true);
+    assert(node.key == ret.key);
+    assert(node.value == ret.value);
+    assert(list.Delete(node) == true);
+    assert(list.Search(node, ret) == false);
+    std::cout<<std::to_string(r)<<std::endl;
+  }
+}
+
+void MultiThRandomSearchTest() {
+  std::vector<std::thread> thread_vec;
+  for (int i = 0; i < 10; i++) {
+    thread_vec.emplace_back(&RandomSearchTest, i);
+  }
+  for (auto& each : thread_vec) {
+    each.join();
+  }
+}
+
+int main() {
+  MultiThRandomSearchTest();
+  return 0;
+}
